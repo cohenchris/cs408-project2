86c508ab7c9b040fd1fcc23b686ff60256caf4e0
cohenchris <cohen50@purdue.edu>
2020-11-09 23:00:35 -0500

    original functions called and logged

--- skeleton/testlib.c
+++ skeleton/testlib.c
@@ -20,82 +20,100 @@ typedef int (*pthread_mutex_trylock_type)();
 // Thread Management
 int pthread_create(pthread_t *thread, const pthread_attr_t *attr,
                    void *(*start_routine) (void *), void *arg) {
+  INFO("CALL pthread_create(%x, %x, %x, %x)\n", thread, attr, start_routine, arg);
 
-  // TODO: Implement;
-
-  INFO("Called pthread_create\n");
-
-  //This is an example of calling the original function. You may need to change this in your implementation
   pthread_create_type orig_create;
   orig_create = (pthread_create_type)dlsym(RTLD_NEXT, "pthread_create");
 
-  return orig_create(thread, attr, start_routine, arg);
-
+  int return_val = orig_create(thread, attr, start_routine, arg);
+  INFO("RETURN pthread_create(%x, %x, %x, %x) = %d\n", thread, attr, start_routine, arg, return_val);
+  return return_val;
 }
 
 void pthread_exit(void *retval) {
-  INFO("Called pthread_exit");
+  INFO("CALL pthread_exit(%x)\n", retval);
   
   pthread_exit_type orig_exit;
   orig_exit = (pthread_exit_type)dlsym(RTLD_NEXT, "pthread_exit");
 }
 
 int pthread_yield(void) {
-  INFO("Called pthread_yield");
+  INFO("CALL pthread_yield()\n");
 
   pthread_yield_type orig_yield;
   orig_yield = (pthread_yield_type)dlsym(RTLD_NEXT, "pthread_yield");
-  return 0;
+
+  int return_val = orig_yield();
+  INFO("RETURN pthread_yield() = %d\n", return_val);
+  return return_val;
 }
 
 // Condition variables
 int pthread_cond_wait(pthread_cond_t *cond, pthread_mutex_t *mutex) {
-  INFO("Called pthread_cond_wait");
+  INFO("CALL pthread_cond_wait(%x, %x)\n", cond, mutex);
   
   pthread_cond_wait_type orig_cond_wait;
   orig_cond_wait = (pthread_cond_wait_type)dlsym(RTLD_NEXT, "pthread_cond_wait");
-  return 0;
+
+  int return_val = orig_cond_wait(cond, mutex);
+  INFO("RETURN pthread_cond_wait(%x, %x) = %d\n", cond, mutex, return_val);
+  return return_val;
 }
 
 int pthread_cond_signal(pthread_cond_t *cond) {
-  INFO("Called pthread_cond_signal");
+  INFO("CALL pthread_cond_signal(%x)\n", cond);
   
   pthread_cond_signal_type orig_cond_signal;
   orig_cond_signal = (pthread_cond_signal_type)dlsym(RTLD_NEXT, "pthread_cond_signal");
-  return 0;
+
+  int return_val = orig_cond_signal(cond);
+  INFO("RETURN pthread_cond_signal(%x) = %d\n", cond, return_val);
+  return return_val;
 }
 
 int pthread_cond_broadcast(pthread_cond_t *cond) {
-  INFO("Called pthread_cond_broadcast");
+  INFO("CALL pthread_cond_broadcast(%x)\n", cond);
   
   pthread_cond_broadcast_type orig_cond_broadcast;
   orig_cond_broadcast = (pthread_cond_broadcast_type)dlsym(RTLD_NEXT, "pthread_cond_broadcast");
-  return 0;
+
+  int return_val = orig_cond_broadcast(cond);
+  INFO("RETURN pthread_cond_broadcast(%x) = %d\n", cond, return_val);
+  return return_val;
 }
 
 // Mutexes
 int pthread_mutex_lock(pthread_mutex_t *mutex) {
-  INFO("Called pthread_mutex_lock");
+  INFO("CALL pthread_mutex_lock(%x)\n", mutex);
 
   pthread_mutex_lock_type orig_mutex_lock;
   orig_mutex_lock = (pthread_mutex_lock_type)dlsym(RTLD_NEXT, "pthread_mutex_lock");
-  return 0;
+
+  int return_val = orig_mutex_lock(mutex);
+  INFO("RETURN pthread_mutex_lock(%x) = %d\n", mutex), return_val;
+  return return_val;
 }
 
 int pthread_mutex_unlock(pthread_mutex_t *mutex) {
-  INFO("Called pthread_mutex_unlock");
+  INFO("CALL pthread_mutex_unlock(%x)\n", mutex);
   
   pthread_mutex_unlock_type orig_mutex_unlock;
   orig_mutex_unlock = (pthread_mutex_unlock_type)dlsym(RTLD_NEXT, "pthread_mutex_unlock");
-  return 0;
+
+  int return_val = orig_mutex_unlock(mutex);
+  INFO("RETURN pthread_mutex_unlock(%x) = %d\n", mutex, return_val);
+  return return_val;
 }
 
 int pthread_mutex_trylock(pthread_mutex_t *mutex) {
-  INFO("Called pthread_mutex_trylock");
+  INFO("CALL pthread_mutex_trylock(%x)\n", mutex);
   
   pthread_mutex_trylock_type orig_mutex_trylock;
   orig_mutex_trylock = (pthread_mutex_trylock_type)dlsym(RTLD_NEXT, "pthread_mutex_trylock");
-  return 0;
+
+  int return_val = orig_mutex_trylock(mutex);
+  INFO("RETURN pthread_mutex_trylock(%x) = %d\n", mutex, return_val);
+  return return_val;
 }
 
 // This will get called at the start of the target programs main function
