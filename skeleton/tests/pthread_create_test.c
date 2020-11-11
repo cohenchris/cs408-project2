de7d744e7a6e242dd4eebe6260852eb3abc9568c
cchan207 <chan207@purdue.edu>
2020-11-10 21:41:38 -0500

    Compiled pthread_create test

--- skeleton/tests/pthread_create_test.c
+++ skeleton/tests/pthread_create_test.c
@@ -5,22 +5,23 @@
 struct multipliers {
     int x;
     int y;
-}
+};
 
 void* multiply(void * args) {
-  int result = (args->x) * (args->y);
-  printf("%d * %d = %d", x, y, result);
+  struct multipliers *arguments = (struct multipliers *)args;
+  int result = (arguments->x) * (arguments->y);
+  printf("%d * %d = %d\n", arguments->x, arguments->y, result);
   return (void*)0;
 }
 
 int main() {
   pthread_t thread;
-  
+
   struct multipliers args;
   args.x = 5;
   args.y = 7;
 
-  pthread_create(&thread, NULL, &foo, (void *)&args);
+  pthread_create(&thread, NULL, &multiply, (void *)&args);
   sleep(1);
   return 0;
 }
