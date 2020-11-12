762ab8942106f28ada7e960255a9982d800255ef
cohenchris <cohen50@purdue.edu>
2020-11-12 16:56:33 -0500

    modified tests.py to cover every argument possibility, included MAX_RUN and MAX_SEED for the user to pick max number of times run and max seed, respectively

--- skeleton/tests.py
+++ skeleton/tests.py
@@ -9,4 +9,29 @@ import os
 import subprocess
 
 # This is only a placeholder, you can do much better!
-rc = subprocess.run(["python3", "coverage.py"])
+#rc = subprocess.run(["python3", "coverage.py"])
+
+# +-----------+-------------------+-------------------+
+# | flag      |   description     |       input       |
+# +-----------+-------------------+-------------------+
+# | -s        | scheduling policy | none, pct, random |
+# | -n        | # times to run    | <some int > 0>    |
+# | -st       | stacktrace enable | true, false       |
+# | -seed     | seed for tester   | <some int>        |
+# +-----------+-------------------+-------------------+
+
+SCHEDULING_POLICIES = ["none", "pct", "random"] # -s    (Different vals for the scheduling policy)
+MAX_RUN = 2                                     # -n    (Max number of times to run)
+STACKTRACE_VALS = ["true", "false"]             # -st   (enable/disable stack tracing)
+MAX_SEED = 5                                    # -seed (Max seed to use)
+
+return_codes = []                               # Will end up being an array of all return codes
+
+for seed in range(MAX_SEED):
+    for run in range(MAX_RUN):
+        for stacktrace in STACKTRACE_VALS:
+            for scheduling_policy in SCHEDULING_POLICIES:
+                iterations += 1
+                run_command = ["python3", "coverage.py", "-s", f"{scheduling_policy}", "-n", f"{run}", "-st", f"{stacktrace}", "-seed" , f"{seed}"]
+                rc = subprocess.run(run_command)
+                return_codes.append(rc.returncode)
